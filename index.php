16c709cb17b0aeb0906d9a9cef26050c9d799727
Maarten Olijve <maartenolijve1@hotmail.com>
2018-10-17 00:19:25 +0200

    Begin index.php, folderstructuur gemaakt, database connectie toegevoegd

--- index.php
+++ index.php
@@ -1,9 +1,62 @@
+<!DOCTYPE html>
 <html>
+<head>
+	<link href="style/style.css" rel="stylesheet" type="text/css" media="all"/>
+	<title>Insta-Food</title>
+</head>
 <body>
-<?php
-for ($i=0;$i<=11;$i++){
-	echo("hello ".$i."</br>");
-}
-?>
+  <h3>Testdata</h3>
+  <?php
+    include "core/database.php";
+
+		$query = 'SELECT * FROM project.users;';
+    $array = mysqli_query($db,$query) or die("error");
+
+    while ($row = mysqli_fetch_assoc($array)) {
+      echo ("<h3>Gebruiker ".$row["id"]."</h3>");
+      echo ("ID = ".$row["id"]."<p>");
+      echo ("Username = ".$row["username"]."<p>");
+      echo ("Password = ".$row["password"]."<p>");
+      echo ("Email = ".$row["email"]."<p>");
+      echo ("Level = ".$row["level"]."<p>");
+    }
+   ?>
 </body>
 </html>
+
+<!--
+##############################################
+## Data die aan het database is toegevoegd: ##
+##############################################
+  users:
+INSERT INTO users (username,password,email,level) VALUES ('testuser','testpass','test@gmail.com',1);
+INSERT INTO users (ID,username,password,email,level) VALUES (1,'barrie','badslipper','barrie@badslipper.nl',9001);
+
+  posts
+[SQL querie]
+
+  category
+[SQL querie]
+
+  hashtags
+[SQL querie]
+
+  message
+[SQL querie]
+
+##############################################
+###### PHP voor printen userdata in body #####
+##############################################
+$query = 'SELECT * FROM project.users;';
+$array = mysqli_query($db,$query) or die("error");
+
+while ($row = mysqli_fetch_assoc($array)) {
+  echo ("<h3>Gebruiker ".$row["id"]."</h3>");
+  echo ("ID = ".$row["id"]."<p>");
+  echo ("Username = ".$row["username"]."<p>");
+  echo ("Password = ".$row["password"]."<p>");
+  echo ("Email = ".$row["email"]."<p>");
+  echo ("Level = ".$row["level"]."<p>");
+}
+
+-->
