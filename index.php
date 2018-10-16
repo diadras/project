32bb6ed529d4aea4a90f0d2d6f9ab15f7afa49d3
Maarten Olijve <maartenolijve1@hotmail.com>
2018-10-17 01:34:26 +0200

    Opnieuw SQL gegenereerd in Workbench en er komt nu een fout, morgen bij database even navragen

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
   <h3>Testdata</h3>
   <?php
     include "./core/database.php";
-
+		// Omdat er een fout is in het database werkt onderstaande query en echo niet
+		//$query = "SELECT username, photodata, recipe FROM project.posts JOIN users USING (gebruiker_id);";
 		$query = "SELECT * FROM project.posts;";
-    $array = mysqli_query($db,$query) or die("error");
+		$array = mysqli_query($db,$query) or die("error");
 
     while ($row = mysqli_fetch_assoc($array)) {
       echo ("<img src=\"".$row["photodata"]."\"/><p>");
 			echo ("Recept: ".$row["recipe"]."<p>");
-			echo ("Gemaakt door gebruiker ID: ".$row["gebruikers_id"]."<p>");
+			//echo ("Gemaakt door gebruiker: ".$row["username"]."<p>");
     }
 
 		// Je kan beter foto opslaan op de schijf zelf en een pointer in "postdata" zetten.
@@ -23,7 +24,7 @@
 		$base64data = file_get_contents("./data/testuser/base64/image1");
 		// Wanneer ik de onderste query met backtics `` invoer krijg ik een kolomfout
 		// Als ik daarna deze vervang met enkele quotes '' gaat alles goed
-		$query2 = "INSERT INTO posts (photodata,recipe,gebruikers_id) VALUES ('./data/testuser/img/600x500.png','Nog geen recept','1');";
+		$query2 = "INSERT INTO posts (photodata,recipe,gebruiker_id) VALUES ('./data/testuser/img/600x500.png','Nog geen recept','1');";
 		echo ($query2);
 		//mysqli_query($db,$query2) or die("error");
 
@@ -37,11 +38,11 @@
 ## Data die aan het database is toegevoegd: ##
 ##############################################
   users:
-INSERT INTO users (username,password,email,level) VALUES (`testuser`,`testpass`,`test@gmail.com`,1);
-INSERT INTO users (username,password,email,level) VALUES (`barrie`,`badslipper`,`barrie@badslipper.nl`,9001);
+INSERT INTO users (username,password,email,level) VALUES ('testuser','testpass','test@gmail.com',1);
+INSERT INTO users (username,password,email,level) VALUES ('barrie','badslipper','barrie@badslipper.nl',9001);
 
   posts
-INSERT INTO posts (photodata,recipe,gebruikers_id) VALUES ('./data/testuser/img/600x500.png','Nog geen recept','1');
+INSERT INTO posts (photodata,recipe,gebruiker_id) VALUES ('./data/testuser/img/600x500.png','Nog geen recept','1');
 
   category
 [SQL querie]
