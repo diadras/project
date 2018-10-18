81fdc212be6acba0f4f96d5987187ed52c353c28
Maarten Olijve <maartenolijve1@hotmail.com>
2018-10-18 13:39:11 +0200

    landingpage die plaatje, recept en username toont van posts

--- index.php
+++ index.php
@@ -8,28 +8,15 @@
   <h3>Testdata</h3>
   <?php
     include "./core/database.php";
-		// Omdat er een fout is in het database werkt onderstaande query en echo niet
-		//$query = "SELECT username, photodata, recipe FROM project.posts JOIN users USING (gebruikers_id);";
-		$query = "SELECT * FROM project.posts;";
+		$query = "SELECT photodata, recipe, username FROM posts JOIN users USING (id);";
 		$array = mysqli_query($db,$query) or die("error");
 
     while ($row = mysqli_fetch_assoc($array)) {
-			echo ("gebruiker: ".$row["gebruikers_id"]);
       echo ("<img src=\"".$row["photodata"]."\"/><p>");
 			echo ("Recept: ".$row["recipe"]."<p>");
-			//echo ("Gemaakt door gebruikers: ".$row["username"]."<p>");
+			// ucfirst() zorgt voor uppercase i.v.m. naam van user
+			echo ("Eigenaar: ".ucfirst($row["username"])."<p>");
     }
-
-		// Je kan beter foto opslaan op de schijf zelf en een pointer in "postdata" zetten.
-		// Fotos opslaan in Base64 in een database KAN, maar is niet praktisch
-		$base64data = file_get_contents("./data/testuser/base64/image1");
-		// Wanneer ik de onderste query met backtics `` invoer krijg ik een kolomfout
-		// Als ik daarna deze vervang met enkele quotes '' gaat alles goed
-		$query2 = "INSERT INTO users (username,password,email,level) VALUES ('testuser','testpass','test@gmail.com',1);
-		INSERT INTO users (username,password,email,level) VALUES ('barrie','badslipper','barrie@badslipper.nl',9001);
-		INSERT INTO posts (photodata,recipe,gebruikers_id) VALUES ('./data/testuser/img/600x500.png','Nog geen recept','1');";
-		//mysqli_query($db,$query2) or die("error");
-		mysqli_query($db,$query2) or die (mysqli_error($db));
 		mysqli_close($db);
 	 ?>
 </body>
