40f042e00cda769ebd4dc992dae8e3c099145f3d
Akoens <arjan@koens.biz>
2018-10-29 14:52:45 +0100

    porfiel foto
    nieuwe foto

---
+++ inlog.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Instafood </title>
+        <link/>
+        <style>
+            div{
+                text-align: center;
+                display: block;
+                background-color: lightgreen;
+            }
+            img{
+                width: 100px;
+                height: 100px;
+            }
+
+        </style>
+    </head>
+    
+    <body>
+        <div>
+        <img src="./img/profile.png" />
+        
+        <form method="POST">
+            <p>Username: <input type="text" name="user">
+            <p>Password: <input type="password"  name="pass"> 
+        </form>
+        </div>
+        <?php 
+    
+
+        ?>
+    </body>
+</html>
