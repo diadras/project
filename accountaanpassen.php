ce83b7c629227a76493626a1b2be82668d9539fc
Anna <annavanrosmalen3@gmail.com>
2018-10-30 16:01:35 +0100

    Update accountaanpassen.php

--- accountaanpassen.php
+++ accountaanpassen.php
@@ -6,7 +6,7 @@
     include "./core/database.php";
 	error_reporting(-1);
   	ini_set('display_errors', 'On');
-  
+
 
 	?>
 	<link href="./style/style.css" rel="stylesheet" type="text/css" media="all"/>
@@ -14,11 +14,12 @@
 </head>
 <body>
 
-  <h3>Account informatie aanpassen</h3>
+  
   <form action="accountaanpassen.php" method="post">
     <table>
+      <th>Account informatie aanpassen</th>
   	<tr><td>Gebruikersnaam:</td><td> <input type="text" name="gebruikersnaam"></td></tr><br>
-  	<tr><td>Wachtwoord:</td><td> <input type="text" name="wachtwoord"></td></tr><br>
+  	<tr><td>Wachtwoord:</td><td> <input type="password" name="wachtwoord"></td></tr><br>
   	<tr><td>E-mailadres:</td><td> <input type="text" name="email"></td></tr><br>
   </table>
   <input type="submit" name="submit" value="Gegevens wijzigen"><br>
