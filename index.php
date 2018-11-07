582596bc63e0a32b033d3f25c71efffac1b6dfee
diadras <m.olijve@st.hanze.nl>
2018-11-07 15:22:44 +0100

    login knop aangepast, deze was foutief aangemaakt

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 			<img src="./img/Logo.png" style="height: 100px" title="Instafood"/>
 		</a>
 		<div class="buttons">
-			<button name="login" onclick="window.location.href='/project/inlog.php'"> login </button> 
+			<button name="login" onclick="window.location.href='./inlog.php'"> login </button> 
 		</div>
 	</div>
 	<?php
@@ -34,7 +34,7 @@
 			echo ("Eigenaar: ".ucfirst($row["username"])."<p>");
 			echo ("</div>");
 		}
-		
+
 		mysqli_close($db);
 	 ?>
 </body>
